66f779615c57fa1cef301566b2b54c13627068b7
clayriley <riley@brandeis.edu>
2016-11-21 15:08:33 -0500

    Created files for various classes used in our system design

---
+++ DBManager.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+This class has been added to provide a single point of access for all other 
+components to look into the databases.
+"""
+
+class DBManager():
+    
+    pass
