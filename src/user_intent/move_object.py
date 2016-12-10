73b422d3375b45fb81a2178a1b2de1be0e651f8e
Yuzhe <yzchen@brandeis.edu>
2016-12-10 12:50:41 -0500

    implement move intent

--- src/user_intent/move_object.py
+++ src/user_intent/move_object.py
@@ -1,2 +1,8 @@
+#!/usr/bin/env python
 class MoveObject(IntentObject):
-	pass
+    def __init__(self, intent, country=None):
+        super(MoveObject, self).__init__(intent)
+        self.country = country
+
+    def is_complete(self):
+        return self.
