71ef740a1a3e208889f9a6942c2494cbb4f17822
Yuzhe <yzchen@brandeis.edu>
2016-12-10 12:30:23 -0500

    add intent abstract class

--- src/user_intent/intent_object.py
+++ src/user_intent/intent_object.py
@@ -1,2 +1,15 @@
+from abc import ABCMeta, abstractmethod, abstractproperty
+
 class IntentObject():
-	pass
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, intent):
+        self.intent = intent
+
+    def get_intent(self):
+        return self.intent
+
+    @abstractmethod
+    def is_complete(self):
+        raise NotImplementedError("is_complete is not implemented")
