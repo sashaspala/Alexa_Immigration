f2c1f9679c1c4040fc4f5c617f1db0733caa2ab3
Yuzhe <yzchen@brandeis.edu>
2016-12-11 13:33:59 -0500

    basic context manager structure

---
+++ src/context_manager/context_manager.py
@@ -0,0 +1,16 @@
+#TODO import query manager
+# Or: talk to the database directly?
+
+class ContextManager:
+    def __init__(self):
+        #TODO init query manager
+        pass
+
+    def getContext(self, intentObj):
+        # fill in missing slots
+        # return intentObj
+        pass
+
+    def updateContext(self):
+        # update context info in database
+        pass
